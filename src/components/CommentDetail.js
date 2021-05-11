39abd7c751f23b6bbcdc75196876f84b0ddfb766
Dan Chiev <danchiev2@Gmail.com>
2021-05-11 18:15:02 -0400

    added props

--- src/components/CommentDetail.js
+++ src/components/CommentDetail.js
@@ -1,20 +1,20 @@
 import React from "react";
 import faker from "faker";
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.image} />
       </a>
       <div className="content">
         <a href="/" className="author">
-          {faker.name.findName()}
+          {props.author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6PM</span>
+          <span className="date">{props.timeAgo}</span>
         </div>
-        <div className="text">Nice BLog post</div>
+        <div className="text">{props.text}</div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -6,7 +6,18 @@ import CommentDetail from "./components/CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <CommentDetail
+        author="Sam"
+        timeAgo={faker.time.recent()}
+        text={faker.lorem.text()}
+        image={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo={faker.time.recent()}
+        text={faker.lorem.text()}
+        image={faker.image.avatar()}
+      />
     </div>
   );
 };
