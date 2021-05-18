9cd7ff7bd3e9664c943a7c7d0a34ec4a61ae7cec
Dan Chiev <danchiev2@Gmail.com>
2021-05-18 15:11:11 -0400

    now shows children content

--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ import CommentDetail from "./components/CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        timeAgo={faker.time.recent()}
-        text={faker.lorem.text()}
-        image={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo={faker.time.recent()}
+          text={faker.lorem.text()}
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
       <CommentDetail
         author="Alex"
         timeAgo={faker.time.recent()}
