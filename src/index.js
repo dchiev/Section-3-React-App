09fe52b9fa21009d35fc67f7102904ec1d0ad4be
Dan Chiev <danchiev2@Gmail.com>
2021-05-18 15:06:41 -0400

    initial strucutre

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
-
+import ApprovalCard from "./components/ApprovalCard";
 import CommentDetail from "./components/CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard />
       <CommentDetail
         author="Sam"
         timeAgo={faker.time.recent()}
